bc4e5af5c3162e3318c95174e5c6a0c16884b1e7
Tavinzera <santosteixeira.otavioaugusto@gmail.com>
2026-08-12 23:41:06 -0300

    big update

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-loadTemplates();
+const reload = () => {
+    loadTemplates();
+    lucide.createIcons();
+    cardListen();
+}
 
 const dropdownOpn = document.querySelector('.options');
 const dpBtn = document.querySelector('.dropdownBtn');
@@ -45,17 +49,16 @@ colors.forEach((color) => {
 const createTemplate = document.querySelector('.create-template')
 const formTemplate = document.querySelector('.form')
 createTemplate.addEventListener('click', () => {
-    formTemplate.classList.add('active')
+    blockBg("on", formTemplate, "active")
 })
 
-const closeBtn = document.querySelector('.closeBtn')
+const closeBtn = document.getElementById('closeForm')
 closeBtn.addEventListener('click', () => {
-    formTemplate.classList.remove('active')
+    blockBg("off", formTemplate, "active")
 })
 
 formTemplate.addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log("SUBMIT FUNCIONOU!")
     const title = document.querySelector('.title-template').value
     const template = {
         id: crypto.randomUUID(),
@@ -65,26 +68,63 @@ formTemplate.addEventListener('submit', (event) => {
         color: colorSelected,
         sets: []
     }
-    console.log(template);
-    formTemplate.classList.remove('active')
+    blockBg("off", formTemplate, "active")
     const templates = JSON.parse(localStorage.getItem("templates")) || [];
     templates.push(template)
     localStorage.setItem("templates", JSON.stringify(templates));
-    console.log(template)
-    loadTemplates();
     formTemplate.reset();
+    reload();
 })
+
+const delTemplateBtn = document.querySelector('.delTemplate')
+const delBtns = document.querySelectorAll('.delButton')
+const cardListen = () => {
+    const cards = document.querySelectorAll('.templateCard');
+    const del = document.querySelector('.delete');
+    cards.forEach((card) => {
+    if (card.id !== "demo") {
+        card.addEventListener('click', () => {
+        const cardId = card.dataset.id
+        if(event.target.closest('.delete')){
+            blockBg("on", delTemplateBtn, "active")
+            const closeBtnDel = document.getElementById('closeDel')
+            closeBtnDel.addEventListener(('click'), () => {
+                blockBg("off", delTemplateBtn, "active")
+            })
+            delBtns.forEach((delBtn) => {
+                delBtn.addEventListener('click', () => {
+                    if(delBtn.id === "confirm"){
+                        const templates = JSON.parse(localStorage.getItem("templates"));
+                        const newTemplates = templates.filter((template) => {
+                            return template.id !== cardId;
+                        })
+                        localStorage.setItem("templates", JSON.stringify(newTemplates));
+                        reload()
+                        blockBg("off", delTemplateBtn, "active")
+                    } else {
+                        blockBg("off", delTemplateBtn, "active")
+                    }
+                })
+            } )
+            return
+        }    
+        window.location.href = `board.html?id=${cardId}`
+        
+    })
+    }
+})}
 
 function loadTemplates() {
     const templates = JSON.parse(localStorage.getItem("templates")) || [];
     const container = document.querySelector(".templateShowing")
     container.innerHTML = "";
     templates.forEach((template) => {
-        const card = `
+        const card = `  
             <div class="templateCard" data-id="${template.id}">
               <div class="backgroundColor" style="background-color: ${template.color}">
                 <a class="iconAnchor">
-                  <i data-lucide="external-link" class="templateIcon"></i>
+                  <i data-lucide="trash-2" class="templateIcon delete" id="demo"></i>
+                  <i data-lucide="external-link" class="templateIcon share"></i>
                 </a>
               </div>
               <h1 class="templeTitle">${template.title}</h1>
@@ -95,15 +135,25 @@ function loadTemplates() {
     })
 }
 
-const cards = document.querySelectorAll('.templateCard')
-cards.forEach((card) => {
-    if (card.id === "demo") {
-        return
-    } else {
-    card.addEventListener('click', () => {
-        const cardId = card.dataset.id      
-        window.location.href = `board.html?id=${cardId}`
-    })}
+const blockBg = (turn, element, type) => {
+    const block = document.querySelector('.block')
+    if(turn === "on"){
+        block.classList.add("on")
+        element.classList.add(type)
+        block.addEventListener('click', (event) => {
+        if(event.target === block){
+            element.classList.remove(type)
+            block.classList.remove(turn)
+        }
+    })
+    } if(turn === "off") {
+        block.classList.remove("on")
+        element.classList.remove(type)
+    }
+}
+
+delTemplateBtn.addEventListener(('click'), () => {
+
 })
 
-lucide.createIcons();
+reload()
